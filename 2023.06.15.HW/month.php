0d1205d0fb19557e2f62a6b53c1cbedf3c2a00d8
Rhoxolan <pba17mail@gmail.com>
2024-07-19 00:03:39 +0300

    The home work from the third lesson was completed

--- 2023.06.15.HW/month.php
+++ 2023.06.15.HW/month.php
@@ -4,6 +4,8 @@ function generateMonthCalendar($m) {
         throw new Exception('The value $m isn`t valid!');
     }
 
+    $today = date("j"); //Получаем текущий день
+    $month = date("m"); //Получаем текущий месяц
     $year = date("Y"); //Получаем год
     $firstDayOfMonthTimestamp  = mktime(0, null, null, $m, 1, $year); //Получаем временную метку Unix для первого дня месяца
     $daysInMonth = date("t", $firstDayOfMonthTimestamp); //Получаем к-во дней в месяце
@@ -25,15 +27,20 @@ function generateMonthCalendar($m) {
     while ($currentDay <= $daysInMonth) {
         if ($dayOfWeek == 7) {
             $dayOfWeek = 0;
-            $component .= "</tr><tr>";
+            $component .= "</tr><tr>"; //Размечаем строку для дней месяца
         }
-        $component .= "<td>$currentDay</td>";
+        if ($currentDay == $today && $m == $month) {
+            $component .= "<td><strong>$currentDay</strong></td>"; //Добавляем день месяца
+        }
+        else {
+            $component .= "<td>$currentDay</td>"; //Добавляем день месяца
+        }
         $currentDay++;
         $dayOfWeek++;
     }
     if ($dayOfWeek != 7) {
         for ($i = $dayOfWeek; $i < 7; $i++) {
-            $component .= "<td></td>";
+            $component .= "<td></td>"; //Если день не = Воскресенье - добавляем пустые ячейки
         }
     }
     $component .= "</tr></table>";
