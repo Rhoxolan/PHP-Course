03c58b6accbe4a61357e48dd725c4827e553faab
Rhoxolan <pba17mail@gmail.com>
2024-07-28 00:07:39 +0300

    The home work from the fourth lesson was supplemented

--- 2023.06.16.HW/addCar.php
+++ 2023.06.16.HW/addCar.php
@@ -1,1 +1,31 @@
-<h1>Add Car</h1>
+<form action="addCar.server.php" method="post">
+    <div style="margin: 5px;">
+        <div>
+            <label for="Manufacturer_input">Manufacturer:</label>
+        </div>
+        <div>
+            <input id="Manufacturer_input" name="Manufacturer" type="text" required />
+        </div>
+        <div>
+            <label for="Model_input">Model:</label>
+        </div>
+        <div>
+            <input id="Model_input" name="Model" type="text" required />
+        </div>
+        <div>
+            <label for="Year_input">Year:</label>
+        </div>
+        <div>
+            <input id="Year_input" name="Year" type="number" min="1900" max="2024" step="1" required />
+        </div>
+        <div>
+            <label for="Price_input">Price:</label>
+        </div>
+        <div>
+            <input id="Price_input" name="Price" type="number" required />
+        </div>
+    </div>
+    <div style="margin: 5px;">
+        <input style="margin-top: 5px;" type="submit" name="Send" value="Send" />
+    </div>
+</form>
---
+++ 2023.06.16.HW/addCar.server.php
@@ -0,0 +1,11 @@
+<?
+$car = array(
+    "manufacturer" => $_POST["Manufacturer"],
+    "model" => $_POST["Model"],
+    "price" => $_POST["Price"],
+    "year" => $_POST["Year"]
+);
+
+function SaveToFile($car)
+{
+}
